97737e05328952b3958f39fa7fb3d73abf90ea2f
JacobScott98 <113859725+JacobScott98@users.noreply.github.com>
2022-11-01 19:01:43 -0600

    Update cleanData.scala

--- Steamboat Weather/src/main/scala/com/weather/spark/cleanData.scala
+++ Steamboat Weather/src/main/scala/com/weather/spark/cleanData.scala
@@ -37,22 +37,24 @@ object cleanData {
     val dataFiltered = data_dates.filter(data_dates("Year") > "1908")
 
     // Checking to try and figure out what the "T" values mean in the snow category
-    //dataFiltered.filter(col("Snow") === "T").show() // Looks like all of these values occur during summer months, going to assume these can be changed to zeros
+    dataFiltered.filter(col("Snow") === "T").show() // Looks like all of these values occur during summer months, going to assume these can be changed to zeros
 
     // Replacing the T's with 0's
     val removeT = dataFiltered.withColumn("Snow", when(dataFiltered("Snow") === "T", "0").otherwise(dataFiltered("Snow")))
       .select("Year", "Month", "Max", "Min","Precip", "Snow")
 
+    // Next I wanted to remove the missing "M" values and replace them with the average value for that month across the entire dataset. 
+    // I know this isn't the most sophisticated method, but I mainly wanted to test my ability using basic Spark SQL functions
+    
     // Removing M's and casting types to Integers to get average values
     val data4avg = removeT.filter(removeT("Max") =!= "M" && removeT("Min") =!= "M"  && removeT("Precip") =!= "M" && removeT("Snow") =!= "M")
       .select(col("Year"),col("Month"),col("Max").cast("int"),col("Min").cast("int"),col("Precip").cast("int"),col("Snow").cast("int"))
 
-    // Setting up a local array of average values to use for
+    // Setting up a dataset of average values grouped by month to use later
     val avg_values = data4avg.groupBy("Month").agg(avg("Max"),avg("Min"),avg("Precip"),avg("Snow"))
       .sort("Month")
 
-
-    // Replacing "M" values with the average for that month,
+    // Replacing "M" values with the average for that month
 
     // Joining average values onto the cleaned data, so we can replace the M's
     val avg_joined = removeT.join(avg_values,data4avg("Month") === avg_values("Month"),"left").sort(data4avg("Year"))
@@ -67,7 +69,7 @@ object cleanData {
 
     removeM.show()
 
-    //Output to a csv file for python analysis
+    //Output to a csv file for analysis
     removeM.coalesce(1)
       .write
       .option("header", true)
